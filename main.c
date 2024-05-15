3a03ec75e4caaf9a18f503223bb93c9c99f5ebea
FilipArambasic <filip1022@icloud.com>
2024-05-16 01:07:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "funkcije.h"
+
+int main() {
+
+    int izbor = 1;
+
+    while (izbor != 999) {
+        izbor = izbornik();
+    }
+
+    return 0;
+}
